72e430a46df518c8007d9a30f97b46cc7437ff7b
Stefanos Leventis <sigianas@yahoo.com>
2020-03-11 13:07:43 +0200

    pushed

--- Week1/Homework/project-HackYourRepo/index.js
+++ Week1/Homework/project-HackYourRepo/index.js
@@ -1,54 +1,62 @@
 'use strict';
 
 {
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 299) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
-  }
 
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.entries(options).forEach(([key, value]) => {
-      if (key === 'text') {
-        elem.textContent = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
-    });
-    return elem;
-  }
+    let block = document.querySelector(".info-block")
+    let theRoot = document.querySelector("#root")
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
-  }
+    function fetchJSON(url, cb) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status <= 299) {
+                cb(null, xhr.response)
+                console.log("Loaded!");
+            } else {
+                cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+            }
+        };
+        xhr.onerror = () => cb(new Error('Network request failed'));
+        xhr.send();
+    }
 
-  function main(url) {
-    fetchJSON(url, (err, repos) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, {
-          text: err.message,
-          class: 'alert-error',
+    function createAndAppend(name, parent, options = {}) {
+        const elem = document.createElement(name);
+        parent.appendChild(elem);
+        Object.entries(options).forEach(([key, value]) => {
+            if (key === 'text') {
+                elem.textContent = value;
+                console.log(value)
+            } else {
+                elem.setAttribute(key, value);
+            }
         });
-        return;
-      }
-      const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
-    });
-  }
+        return elem;
+    }
 
-  const HYF_REPOS_URL =
-    'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-  window.onload = () => main(HYF_REPOS_URL);
+    function renderRepoDetails(repo, ul) {
+        createAndAppend('div', theRoot, { text: repo.name });
+
+    }
+
+    function main(url) {
+        fetchJSON(url, (err, repos) => {
+            const root = document.getElementById('root');
+            if (err) {
+                createAndAppend('div', root, {
+                    text: err.message,
+                    class: 'alert-error',
+                });
+                return;
+            }
+
+            const p = createAndAppend('div', root);
+            repos.forEach(repo => renderRepoDetails(repo, p));
+        });
+    }
+
+    const HYF_REPOS_URL =
+        'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+    window.onload = () => main(HYF_REPOS_URL);
 }
