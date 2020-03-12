ad88878a48af965377033508d32bb49e299e2644
Stefanos <sigianas@yahoo.com>
2020-03-12 19:31:49 +0200

    pushed

--- Week1/Homework/project-HackYourRepo/index.js
+++ Week1/Homework/project-HackYourRepo/index.js
@@ -2,7 +2,6 @@
 
 {
 
-    let block = document.querySelector(".info-block")
     let theRoot = document.querySelector("#root")
 
     function fetchJSON(url, cb) {
@@ -13,6 +12,7 @@
             if (xhr.status >= 200 && xhr.status <= 299) {
                 cb(null, xhr.response)
                 console.log("Loaded!");
+                console.log(xhr.response);
             } else {
                 cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
             }
@@ -26,8 +26,7 @@
         parent.appendChild(elem);
         Object.entries(options).forEach(([key, value]) => {
             if (key === 'text') {
-                elem.textContent = value;
-                console.log(value)
+                elem.innerHTML = value;
             } else {
                 elem.setAttribute(key, value);
             }
@@ -35,24 +34,52 @@
         return elem;
     }
 
-    function renderRepoDetails(repo, ul) {
-        createAndAppend('div', theRoot, { text: repo.name });
+    function sortThemAll(repos) {
+        repos.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1);
+    }
 
+    function renderRepoDetails(repo, theRoot) {
+        createAndAppend('section', theRoot, {
+            text: `
+            <div class="prop-text">
+            <p><strong>Repository: </strong></p>
+            <p><strong>Description: </strong></p>
+            <p><strong>Forks: </strong></p>
+            <p><strong>Updated: </strong></p>
+            </div>
+            <div class="prop-value">
+            <p><a href="${repo.html_url}">${repo.name}</a></p>
+            <p>${repo.description}</p>
+            <p>${repo.forks_count}</p>
+            <p>${repo.updated_at.slice(8, 10)}/${repo.updated_at.slice(5, 7)}/${repo.updated_at.slice(0, 4)},${repo.updated_at.slice(12, -1)}</p>
+            </div>
+            `,
+            class: "info-block"
+        });
     }
 
     function main(url) {
+        createAndAppend("header", theRoot, {
+            text: `
+            <h1>HYF Repositories</h1>
+            `
+        })
+
         fetchJSON(url, (err, repos) => {
             const root = document.getElementById('root');
             if (err) {
-                createAndAppend('div', root, {
+                createAndAppend('section', root, {
                     text: err.message,
                     class: 'alert-error',
                 });
                 return;
             }
 
-            const p = createAndAppend('div', root);
-            repos.forEach(repo => renderRepoDetails(repo, p));
+            const section = createAndAppend('section', root);
+            sortThemAll(repos);
+            for (let i = 0; i < 10; i++) {
+                renderRepoDetails(repos[i], section);
+            }
         });
     }
 
